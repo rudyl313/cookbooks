fdc0664a165f277b7ef41b1c7780701258f8fc5a
Rudy Lippert <rudyl313@gmail.com>
2012-10-14 23:35:37 -0700

    Updated Rstudio version

--- R/attributes/default.rb
+++ R/attributes/default.rb
@@ -1,4 +1,4 @@
-set_unless[:R][:rstudio_version] = "0.96.225"
+set_unless[:R][:rstudio_version] = "0.96.331"
 set_unless[:R][:distro] = "precise"
 set_unless[:R][:packages]= []
 set_unless[:R][:cran_mirror] = "http://cran.stat.ucla.edu"
