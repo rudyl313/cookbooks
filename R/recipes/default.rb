6a44b2f52b51bc8a54b67b6063cf3834ebc5c344
Rudiger Lippert <rudyl313@gmail.com>
2013-03-15 19:45:59 -0700

    Worked around some issues in the new chef version

--- R/recipes/default.rb
+++ R/recipes/default.rb
@@ -3,14 +3,22 @@ include_recipe "apt"
 package "libssl0.9.8"
 package "libapparmor1"
 package "apparmor-utils"
+package "build-essential"
 
-apt_repository "cran" do
-  uri "http://cran.opensourceresources.org/bin/linux/ubuntu #{node[:R][:distro]}/"
-  keyserver "keyserver.ubuntu.com"
-  key "E084DAB9"
-  action :add
-  notifies :run, resources(:execute => "apt-get update"), :immediately
-  not_if "[ -f /usr/sbin/rstudio-server ]"
+new_repo = "deb #{node[:R][:cran_mirror]}/bin/linux/ubuntu #{node[:R][:distro]}/"
+
+bash "install cran repository" do
+  user "root"
+  group "root"
+  cwd "/tmp"
+  environment({'USER' => 'root', 'HOME' => '/root'})
+  code <<-CODE
+    gpg --keyserver keyserver.ubuntu.com --recv-key E084DAB9
+    gpg -a --export E084DAB9 | sudo apt-key add -
+    echo '#{new_repo}' >> /etc/apt/sources.list
+    apt-get update
+  CODE
+  not_if "[ -f /usr/bin/R ]"
 end
 
 package "r-base" do
@@ -21,21 +29,17 @@ package "r-base-dev" do
   options "--force-yes"
 end
 
-unless node[:R][:packages].empty?
+template "/tmp/packages.R" do
+  source "packages.R.erb"
+  mode "755"
+  owner "root"
+  group "root"
+end
 
-  template "/tmp/packages.R" do
-    source "packages.R.erb"
-    mode "755"
-    owner "root"
-    group "root"
-  end
-
-  bash "install packages" do
-    user "root"
-    group "root"
-    cwd "/tmp"
-    environment({'USER' => 'root', 'HOME' => '/root'})
-    code "R --vanilla < packages.R"
-  end
-
+bash "install packages" do
+  user "root"
+  group "root"
+  cwd "/tmp"
+  environment({'USER' => 'root', 'HOME' => '/root'})
+  code "R --vanilla < packages.R"
 end
